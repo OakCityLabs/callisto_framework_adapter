870d091f080910b9c04760cfdf5d51b3e6de713d
Jay Lyerly <jay@oakcity.io>
2022-05-09 16:22:32 -0400

    Specify platform support levels.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "callisto_framework_adapter",
+    platforms: [
+        .iOS(.v14), .macCatalyst(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
